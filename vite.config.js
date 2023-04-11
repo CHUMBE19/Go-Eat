95bba089819718323b368b6bb34c13541cd480e6
Salvador Chumbe Cordova <salvdor290915@gmail.com>
2023-04-11 10:07:11 -0500

    my app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Go!Eat/",
+  base:"/Go-Eat/",
   plugins: [svelte()],
 })
